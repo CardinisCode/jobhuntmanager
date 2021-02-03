35bb20b2a8d62db6129a262f989e74d5e8f99536
Andi Folgado <ac.folgado@gmail.com>
2021-02-03 17:34:18 +0000

    Day finished.

--- service/homepage.py
+++ service/homepage.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template, session, flash
 from datetime import datetime, date
 
 
+def combine_date_and_time_into_1_string(interview_date, interview_time): 
+    # I want to format the datetime format to be "%Y-%m-%d %H:%M"
+    # 1) I will take the str values for Date and Time, & combine them into 1 datetime string 
+    date_time_str = interview_date + " " + interview_time
+
+    #2) Now I will convert this datetime string into its datetime object:
+    date_time_format = '%Y-%m-%d %H:%M:%S'
+    date_and_time_obj = datetime.strptime(date_time_str, date_time_format)
+
+    # 3) Now I can convert this datetime_obj into a string & simultaneously get the format I want:
+    updated_date_time = date_and_time_obj.strftime("%m/%d/%Y, %H:%M")
+
+    return updated_date_time
+
+
 def grab_values_from_top_5_interviews_SQLquery_and_return_top_5_interviews_dict(interviewsRepo, user_id):
     top_5_interviews_dict = {}
     query_results = interviewsRepo.grabTop5InterviewsForUser(user_id)
@@ -22,10 +37,13 @@ def grab_values_from_top_5_interviews_SQLquery_and_return_top_5_interviews_dict(
         other_medium = interview[10]
         contact_number = interview[11]
         status = interview[12]
+
+        # Let's combined our date & time values (received from the SQL query) into 1 variable
+        # Using the format: "%Y-M-D, H:M":
+        updated_date_time = combine_date_and_time_into_1_string(interview_date, interview_time)
     
         top_5_interviews_dict[id_count] = {
-            "interview_date": interview_date, 
-            "interview_time": interview_time,
+            "Date & Time": updated_date_time,
             "company_name": interview[1], 
             "job_role": interview[4],
             "interview_type": interview_type,
@@ -37,19 +55,10 @@ def grab_values_from_top_5_interviews_SQLquery_and_return_top_5_interviews_dict(
             "interviewer_names": interviewer_names, 
         } 
 
-        # I want to format the datetime format to be "%Y-%m-%d %H:%M"
-        # 1) I will take the str values for Date and Time, & combine them into 1 datetime string 
-        date_time_str = interview_date + " " + interview_time
 
-        #2) Now I will convert this datetime string into its datetime object:
-        date_time_format = '%Y-%m-%d %H:%M:%S'
-        date_and_time_obj = datetime.strptime(date_time_str, date_time_format)
-
-        # 3) Now I can convert this datetime_obj into a string & simultaneously get the format I want:
-        date_time = date_and_time_obj.strftime("%m/%d/%Y, %H:%M")
 
         # Now I can finish by adding this to my top_5_interviews_dict dictionary: 
-        top_5_interviews_dict[id_count]["scheduled_date"] = date_time
+        top_5_interviews_dict[id_count]["scheduled_date"] = updated_date_time
 
         # Just so that we grab an unique ID for every application on this list:
         id_count += 1
