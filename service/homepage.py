733b41bb41fc6fc1c9a80759d16a42526d30fb29
Andi Folgado <ac.folgado@gmail.com>
2021-02-04 12:51:55 +0000

    Top5Interviews displaying details as desired. Code cleaned up.

--- service/homepage.py
+++ service/homepage.py
@@ -2,6 +2,50 @@ from flask import Flask, render_template, session, flash
 from datetime import datetime, date
 
 
+def cleanup_fields_for_better_display(top_5_interviews_dict, id_count, other_medium):
+    # Let's start by extracting the fields we want to update: 
+    interview_type = top_5_interviews_dict[id_count]["interview_type"]
+    status = top_5_interviews_dict[id_count]["status"]
+    medium = top_5_interviews_dict[id_count]["video_medium"] 
+
+    # Lets set a few conditions for what gets displayed to the user & clean up the presentation of the data:
+    if  interview_type == "video_or_online":
+        top_5_interviews_dict[id_count]["interview_type"] = "Video / Online"
+
+        # At this point we know the user's selected "Video / Online", 
+        # so we can add the medium to the fields displayed in the table to the user:
+        if medium == "skype":
+            medium = "Skype"
+        elif medium == "zoom":
+            medium = "Zoom"
+        elif medium == "google_chat":
+            medium = "Google Chat"
+        elif medium == "meet_jit_si":
+            medium = "Meet.Jit.Si"
+        else: 
+            medium = other_medium
+        top_5_interviews_dict[id_count]["video_medium"] = medium
+
+    elif interview_type == "phone_call": 
+        top_5_interviews_dict[id_count]["interview_type"] = "Phone Call"
+        top_5_interviews_dict[id_count]["video_medium"] = "N/A"
+
+    else: 
+        top_5_interviews_dict[id_count]["interview_type"] = "In Person"
+        top_5_interviews_dict[id_count]["video_medium"] = "N/A"
+
+    if status == "upcoming": 
+        top_5_interviews_dict[id_count]["status"] = "Upcoming"
+    elif status == "done":
+        top_5_interviews_dict[id_count]["status"] = "Interview Done"
+    elif status == "cancelled":
+        top_5_interviews_dict[id_count]["status"] = "Cancelled"
+    else: 
+        top_5_interviews_dict[id_count]["status"] = "Post-poned"
+
+    return top_5_interviews_dict
+
+
 def combine_date_and_time_into_1_string(interview_date, interview_time): 
     # I want to format the datetime format to be "%Y-%m-%d %H:%M"
     # 1) I will take the str values for Date and Time, & combine them into 1 datetime string 
@@ -24,12 +68,15 @@ def grab_values_from_top_5_interviews_SQLquery_and_return_top_5_interviews_dict(
     if not query_results:
         flash("top_5_interviews_query didn't return any data. Please review!")
 
+    # We need some headings for our fields:
+    # I've chosen to use a list instead of an imbedded dict as this prevents the headings from being printed more than once each. 
+    # When using a for loop on the index.html page.
+    top_5_interviews_dict["headings"] = ["Id#", "Date & Time", "Company Name", "Job Role", "Interview Type", "Status", "Video Medium", "Interview Location","Contact No.", "Interviewer Names"]
+
     id_count = 1
     for interview in query_results: 
-        # company_name = interview[1]
         interview_date = interview[2]
         interview_time = interview[3]
-        # job_role = interview[4]
         interviewer_names = interview[5]
         interview_type = interview[7]
         location = interview[8]
@@ -44,30 +91,26 @@ def grab_values_from_top_5_interviews_SQLquery_and_return_top_5_interviews_dict(
 
         # Now I can finish by adding the updated datetime, along with all the other values, 
         # to my top_5_interviews_dict dictionary: 
-
         top_5_interviews_dict[id_count] = {
             "Id#": id_count,
-            "date_&_Time": updated_date_time,
+            "date_&_time": updated_date_time,
             "company_name": interview[1], 
             "job_role": interview[4],
             "interview_type": interview_type,
-            "interview_Location": location,
-            "video_medium": medium,
-            "other_medium": other_medium, 
-            "contact_number": contact_number,
             "status": status,
+            "video_medium": medium,
+            "location": location,
+            "contact_number": contact_number,
             "interviewer_names": interviewer_names, 
         } 
+
+        # Lets clean up how our values are displayed to the user on Index.html:
+        updated_interviews_dict = cleanup_fields_for_better_display(top_5_interviews_dict, id_count, other_medium)
 
         # Just so that we grab an unique ID for every application on this list:
         id_count += 1
     
-    # In order to use a for loop to display the headings in our table on Index
-    # without using a for loop within a for loop as this would print the headings far more times than we need it to
-    # (To get technical this would print n*n headings where n = the no. of headings)
-    top_5_interviews_dict["headings"] = ["Id#", "Date & Time", "Company Name", "Job Role", "Interview Type", "Location", "Video Medium", "Other Medium", "Contact No.", "Status", "Interviewer Names"]
-
-    return top_5_interviews_dict
+    return updated_interviews_dict
 
 
 def create_homepage_content(session, user_id, applicationsRepo, interviewsRepo):
