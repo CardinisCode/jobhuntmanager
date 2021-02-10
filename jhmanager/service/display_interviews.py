bf1c60a3eb6be599f5b56097df8d2ab0fc339c7e
Andi Folgado <ac.folgado@gmail.com>
2021-02-10 14:43:37 +0000

    Played around with display_interviews, found that the data itself is in order & realized that no matter the order in which values are added to the display_dict, they're displayed in the same order. Updated the ordering of the fields in interviews.html for now

--- jhmanager/service/display_interviews.py
+++ jhmanager/service/display_interviews.py
@@ -2,6 +2,68 @@ from flask import Flask, render_template, session, request, redirect
 from datetime import datetime, time
 
 
+def improve_display_values(details_dict, interview_id, other_medium):
+    interview_type = details_dict[interview_id]["interview_type"]
+    medium = details_dict[interview_id]["medium"]
+    interview_status = details_dict[interview_id]["interview_status"]
+    interviewers = details_dict[interview_id]["interviewers"]
+    contact_number = details_dict[interview_id]["contact_number"]
+
+    #1: Lets set a condition: 
+    # If interview_type == "video_or_online": Display medium's value, Else: medium = "N/A"
+    if interview_type == 'video_or_online':
+        # Firstly we improve how the value gets displayed:
+        interview_type = "Video / Online"
+
+        # Secondly let's improve the values' appearance for video medium:
+        if medium == "zoom":
+            medium = "Zoom"
+        elif medium == "skype":
+            medium = "Skype"
+        elif medium == "google_chat":
+            medium = "Google Chat"
+        elif medium == "meet_jit_si":
+            medium = "Meet Jit Si"
+        else:
+            # If user selects 'Other', I want their chosen medium to be displayed under the heading "Video Medium":
+            medium = other_medium
+        details_dict[interview_id]["medium"] = medium
+
+    else: 
+        # If interview_type == "in_person" or "phone_call"
+        details_dict[interview_id]["medium"] = "N/A"
+
+        if interview_type == 'in_person':
+            interview_type = "In Person"
+
+        if interview_type == "phone_call":
+            interview_type = "Contact Number"
+    details_dict[interview_id]["interview_type"] = interview_type
+
+
+    # 2: Let's adjust 'status' data so we can capitalize it before it reaches the table
+    if interview_status == "upcoming":
+        interview_status = "Upcoming"
+    elif interview_status == "done":
+        interview_status = "Done"
+    elif interview_status == 'cancelled':
+        interview_status = "Cancelled"
+    else:
+        interview_status = "Post-poned"
+
+    # Now to finalise I can update the value of interview_status in our dictionary: 
+    details_dict[interview_id]["interview_status"] = interview_status
+
+    # 3: If interviewer_names == "Unknown at present", then the field should be blank:
+    if interviewers == "Unknown at present":
+        details_dict[interview_id]["interviewers"] = ""
+
+    # 4: If contact_number is "N/A", lets rather display a blank field:
+    if contact_number == "N/A":
+        details_dict[interview_id]["contact_number"] = ""
+
+
+
 def display_top_10_interviews_to_interviews_html(session, user_id, interviewsRepo):
     interviews = interviewsRepo.grabTopTenInterviewsForUser(user_id)
 
@@ -29,64 +91,14 @@ def display_top_10_interviews_to_interviews_html(session, user_id, interviewsRep
             "date": details_list[2], 
             "time": details_list[3], 
             "job_role": details_list[4], 
-            "interviewers": interviewers, 
+            "interview_type": interview_type,
+            "medium": medium,
             "location": details_list[8], 
+            "interview_status": interview_status,
             "contact_number": contact_number, 
-            "interview_status": details_list[12]
+            "interviewers": interviewers, 
         }
 
-        #1: Lets set a condition: 
-        # If interview_type == "video_or_online": Display medium's value, Else: medium = "N/A"
-        if interview_type == 'video_or_online':
-            # Firstly we improve how the value gets displayed:
-            interview_type = "Video / Online"
-
-            # Secondly let's improve the values' appearance for video medium:
-            if medium == "zoom":
-                medium = "Zoom"
-            elif medium == "skype":
-                medium = "Skype"
-            elif medium == "google_chat":
-                medium = "Google Chat"
-            elif medium == "meet_jit_si":
-                medium = "Meet Jit Si"
-            else:
-                # If user selects 'Other', I want their chosen medium to be displayed under the heading "Video Medium":
-                medium = other_medium
-            details_dict[interview_id]["medium"] = medium
-
-        else: 
-            # If interview_type == "in_person" or "phone_call"
-            details_dict[interview_id]["medium"] = "N/A"
-
-            if interview_type == 'in_person':
-                interview_type = "In Person"
-
-            if interview_type == "phone_call":
-                interview_type = "Contact Number"
-        details_dict[interview_id]["interview_type"] = interview_type
-
-
-        # 2: Let's adjust 'status' data so we can capitalize it before it reaches the table
-        if interview_status == "upcoming":
-            interview_status = "Upcoming"
-        elif interview_status == "done":
-            interview_status = "Done"
-        elif interview_status == 'cancelled':
-            interview_status = "Cancelled"
-        else:
-            interview_status = "Post-poned"
-
-        # Now to finalise I can update the value of interview_status in our dictionary: 
-        details_dict[interview_id]["interview_status"] = interview_status
-
-        # 3: If interviewer_names == "Unknown at present", then the field should be blank:
-        if interviewers == "Unknown at present":
-            details_dict[interview_id]["interviewers"] = ""
-
-        # 4: If contact_number is "N/A", lets rather display a blank field:
-        if contact_number == "N/A":
-            details_dict[interview_id]["contact_number"] = ""
-
+        improve_display_values(details_dict, interview_id, other_medium)
 
     return render_template("interviews.html", details=details_dict)
