93fd699a62557e59c496cd035494f475fbdc0197
Andi Folgado <ac.folgado@gmail.com>
2021-02-12 14:54:28 +0000

    User Details displaying on userprofile.html as expected.

--- jhmanager/service/create_userprofile_content.py
+++ jhmanager/service/create_userprofile_content.py
@@ -7,8 +7,14 @@ def create_userprofile_content(session, userRepo, user_id):
     email_address = userRepo.getEmailAddressByUserID(user_id)
 
     user_details = {
-        "username": username,
-        "user_email": email_address
+        "username": {
+            "heading": "UserName", 
+            "data": username
+        },
+        "user_email": {
+            "heading": "Email Address", 
+            "data": email_address
+        }            
     }
     
     return render_template("userprofile.html", user_details=user_details)
