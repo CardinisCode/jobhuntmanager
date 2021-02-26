68b540491bc7c47172c6cf5f2b7b7fcb9068ceca
Andi Folgado <ac.folgado@gmail.com>
2021-02-26 15:53:46 +0000

    Added company details on 'view_interview_details.html'.

--- jhmanager/service/display_interview_details.py
+++ jhmanager/service/display_interview_details.py
@@ -2,39 +2,48 @@ from flask import Flask, render_template, session, request, redirect
 from datetime import datetime, time
 
 
+def add_company_details(details, company):
+    details["company_details"] = {
+        "Name" : company.name, 
+        "Description" : company.description, 
+        "Location" : company.location, 
+        "Industry" : company.industry, 
+        "Website" : company.url,
+    }
+
 def cleanup_fields(details, other_medium):
     # I want to clean up a few fields to improve how they're displayed:
-    interview_type = details["fields"]["Type"]
+    interview_type = details["interview_fields"]["Type"]
     # interview_date = details["fields"]["Date"]
     # interview_time = details["fields"]["Time"]
-    interview_medium = details["fields"]["Medium"]
-    interviewer_names = details["fields"]["Interviewer Names"]
+    interview_medium = details["interview_fields"]["Medium"]
+    interviewer_names = details["interview_fields"]["Interviewer Names"]
     # status = details["fields"]["Status"]
 
     if interviewer_names == "Unknown at present":
-        details["fields"]["Interviewer Names"] = ""
+        details["interview_fields"]["Interviewer Names"] = ""
 
     if interview_medium == "google_chat":
-        details["fields"]["Medium"] = "Google Chat"
+        details["interview_fields"]["Medium"] = "Google Chat"
 
     elif interview_medium == "meet_jit_si":
-        details["fields"]["Medium"] = "Meet.Jit.Si"
+        details["interview_fields"]["Medium"] = "Meet.Jit.Si"
 
     elif interview_medium == "other":
-        details["fields"]["Medium"] = other_medium
+        details["interview_fields"]["Medium"] = other_medium
 
     else:
-        details["fields"]["Medium"] = interview_medium.capitalize()
+        details["interview_fields"]["Medium"] = interview_medium.capitalize()
 
     
     if interview_type == "in_person":
-        details["fields"]["Type"] = "In Person / On Site"
+        details["interview_fields"]["Type"] = "In Person / On Site"
 
     elif interview_type == "video_or_online":
-        details["fields"]["Type"] = "Video / Online"
+        details["interview_fields"]["Type"] = "Video / Online"
 
     else:
-        details["fields"]["Type"] = "Phone Call"
+        details["interview_fields"]["Type"] = "Phone Call"
 
 
     return True
@@ -46,7 +55,8 @@ def display_interview_details(session, user_id, interviewsRepo, application_id, 
     for application in application_details:
         company_id = application[2]
 
-    company_name = companyRepo.getCompanyById(company_id).name
+    company = companyRepo.getCompanyById(company_id)
+    company_name = company.name
 
     details = {
         "app_id": application_id, 
@@ -54,22 +64,25 @@ def display_interview_details(session, user_id, interviewsRepo, application_id, 
         "company_name": company_name,
     }
 
-    interview_details = interviewsRepo.grabInterviewByID(interview_id)
-    interview_id = interview_details.interview_id
-    application_id = interview_details.application_id
-    medium = interview_details.medium
-    other_medium = interview_details.other_medium
+    interview = interviewsRepo.grabInterviewByID(interview_id)
+    interview_id = interview.interview_id
+    application_id = interview.application_id
+    medium = interview.medium
+    other_medium = interview.other_medium
 
-    details["fields"] = { 
-        "Date": interview_details.interview_date,
-        "Time": interview_details.interview_time,
-        "Type": interview_details.interview_type,
-        "Status": interview_details.status.capitalize(),
+    details["interview_fields"] = { 
+        "Date": interview.interview_date,
+        "Time": interview.interview_time,
+        "Type": interview.interview_type,
+        "Status": interview.status.capitalize(),
         "Medium": medium, 
-        "Location": interview_details.location,
-        "Interviewer Names": interview_details.interviewer_names
+        "Location": interview.location,
+        "Interviewer Names": interview.interviewer_names
     }
 
     cleanup_fields(details, other_medium)
+
+    add_company_details(details, company)
+
 
     return render_template("view_interview_details.html", details=details)
