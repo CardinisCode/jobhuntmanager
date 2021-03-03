1b8a67f721d98ce816a28a1aa6c95bebfbe05c26
Andi Folgado <ac.folgado@gmail.com>
2021-03-03 16:49:57 +0000

    Adjusted the table format for 'Note Details' table on 'view_notes_details.html.

--- jhmanager/service/display_note_details.py
+++ jhmanager/service/display_note_details.py
@@ -5,6 +5,7 @@ def display_user_note_details(application_id, note_id, userNotesRepo, companyRep
     user_notes = userNotesRepo.getUserNotesByID(application_id, note_id)
     company_id = user_notes.company_id
     company_name = companyRepo.getCompanyById(company_id).name
+    delete_note_url = "/applications/{}/user_notes/{}/delete_note".format(application_id, note_id)
 
     general_details = {
         "application_id": application_id,
@@ -13,9 +14,10 @@ def display_user_note_details(application_id, note_id, userNotesRepo, companyRep
     note_details = {
         "note_id": user_notes.notes_id,
         "Date": user_notes.entry_date,
-        "Company Name": company_name,
+        "company_name": company_name,
         "Subject": user_notes.description, 
         "Note": user_notes.user_notes, 
+        "delete_note_url": delete_note_url
     }
 
     return render_template("view_notes_details.html", note_details=note_details, general_details=general_details)
