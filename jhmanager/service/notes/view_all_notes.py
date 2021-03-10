c848a87222ea0a942d2ad429e9ec8a33f289996f
Andi Folgado <ac.folgado@gmail.com>
2021-03-10 14:56:28 +0000

    Moved 'display user notes' to services/notes and renamed the file.

--- jhmanager/service/notes/view_all_notes.py
+++ jhmanager/service/notes/view_all_notes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, request, redirect
 
+
 def display_all_user_notes(user_id, userNotesRepo, companyRepo):
     note_details = {}
     general_details = {}
