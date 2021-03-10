c2f9c9439087f2105094a61e0f14b5940b79d9e4
Andi Folgado <ac.folgado@gmail.com>
2021-03-10 17:04:21 +0000

    Currently displaying all companies in a bullet list (by their company name) on 'view_address_book' page.

--- jhmanager/service/company/view_all_companies.py
+++ jhmanager/service/company/view_all_companies.py
@@ -5,17 +5,22 @@ def display_all_companies_for_user(user_id, companyRepo, applicationsRepo):
     company_entries = companyRepo.getAllCompanyEntriesForUser(user_id)
     general_details = {}
     general_details["empty_table"] = False
+    general_details["message"] = None
     company_details = None 
 
     if not company_entries:
         general_details["empty_table"] = True
+        general_details["message"] = "No contacts yet. Add your first application."
 
     else: 
         company_details = {}
+        entry_id = 0
         for company in company_entries:
+            entry_id += 1
             company_name = company.name
             company_id = company.company_id
-            company_details[company_id] = {
+            company_details[entry_id] = {
+                "company_id": company_id,
                 "company_name": company_name,
                 "view_more_url": "View more..."
             }
