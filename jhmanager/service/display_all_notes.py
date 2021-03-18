b1c3f32a8495a5d1e4c288d70066e012821737d9
Andi Folgado <ac.folgado@gmail.com>
2021-03-18 16:31:27 +0000

    Added some styling to 'User Notes'.

--- jhmanager/service/display_all_notes.py
+++ jhmanager/service/display_all_notes.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session, request, redirect
+from datetime import datetime, date
 
 
 def display_all_user_notes(user_id, appNotesRepo, companyRepo, applicationsRepo, companyNotesRepo):
@@ -42,6 +43,29 @@ def display_all_user_notes(user_id, appNotesRepo, companyRepo, applicationsRepo,
                 "delete_note_url": delete_note
             }
             app_notes_details["headings"] = ["#", "Entry Date", "Company Name", "Job Role", "Subject"]
+
+
+    entry_note_count = 0
+    if company_notes != None:
+        for company_note in company_notes: 
+            entry_note_count += 1
+            company_notes_details["fields"] = {}
+            company_notes_details["empty_table"] = False
+            company_note_id = company_note.company_note_id
+            company_id = company_note.company_id
+            view_note_url = '/company/{}/company_note/{}/view_note_details'.format(company_id, company_note_id)
+            update_note_url = '/company/{}/company_note/{}/update_note'.format(company_id, company_note_id)
+            delete_note_url = '/company/{}/company_note/{}/delete_note'.format(company_id, company_note_id)
+            company_notes_details["fields"][entry_note_count] = {
+                "company_name": companyRepo.getCompanyById(company_id).name, 
+                "entry_date": company_note.entry_date, 
+                "subject": company_note.subject, 
+                "view_company_note_url": view_note_url, 
+                "update_company_note_url": update_note_url, 
+                "delete_company_note_url": delete_note_url
+            }
+
+            company_notes_details["headings"] = ["#", "Date", "Company Name", "Note Subject"]
 
 
 
