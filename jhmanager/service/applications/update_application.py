4c8feb87f673f021a9bff7e837aa7388d8747356
Andi Folgado <ac.folgado@gmail.com>
2021-03-23 17:27:24 +0000

    Styled 'update job application' form after the 'add job application' form.

--- jhmanager/service/applications/update_application.py
+++ jhmanager/service/applications/update_application.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, session, request, redirect
 
 
 def display_update_application_form(session, user_id, application_id, update_form, company):
-
     fields = {
         "Company Name": company.name,
-        "application_id": application_id
+        "application_id": application_id, 
+        "update_url": '/applications/{}/update_application'.format(application_id)
     }
 
     return render_template("update_application.html", fields=fields, update_form=update_form)
