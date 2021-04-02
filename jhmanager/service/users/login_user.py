71b2f6c934c82003a0f6c3d6d8cc6a1dd854227d
Andi Folgado <ac.folgado@gmail.com>
2021-04-02 15:24:35 +0100

    Fixed a minor bug in the login functionality.

--- jhmanager/service/users/login_user.py
+++ jhmanager/service/users/login_user.py
@@ -16,16 +16,19 @@ def post_login(login_form, userRepo):
     password = login_form.password.data
 
     user_exists = userRepo.getByUserName(username)
+    details = {
+        "register_url": '/register'
+    }
     if not user_exists:
         flash("Username is not existing.")
-        return render_template("login.html", login_form=login_form)
+        return render_template("login.html", login_form=login_form, details=details)
 
     hashed_password = user_exists[2]
     match = sha256_crypt.verify(password, hashed_password)
 
     if not match: 
         flash("Incorrect Username or Password")
-        return render_template("login.html", login_form=login_form)
+        return render_template("login.html", login_form=login_form, details=details)
 
     session["user_id"] = user_exists[0]
 
