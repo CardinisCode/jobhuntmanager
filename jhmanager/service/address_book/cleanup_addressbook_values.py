7baa1c955423726f18fe3b14f053b813e5a8d15c
Andi Folgado <ac.folgado@gmail.com>
2021-04-02 13:39:33 +0100

    Cleaned up thow the contacts' details are presented to both the 'view address book' and 'view contact details' pages.

--- jhmanager/service/address_book/cleanup_addressbook_values.py
+++ jhmanager/service/address_book/cleanup_addressbook_values.py
@@ -4,10 +4,19 @@ from jhmanager.service.contacts_directory.cleanup_contact import cleanup_company
 
 
 def cleanup_contact_fields(contacts_details, contact_id):
+    for heading, value in contacts_details["fields"][contact_id].items():
+        if value == "N/A":
+            contacts_details["fields"][contact_id][heading] = None
+
     # Need to make sure the user has capitalised the first letter of every name they've provided:
     contacts_details["fields"][contact_id]["full_name"] = cleanup_full_name(contacts_details["fields"][contact_id]["full_name"] )
 
     # Now to do the same for the company name the user provided:
     contacts_details["fields"][contact_id]["company_name"] = cleanup_company_name(contacts_details["fields"][contact_id]["company_name"])
 
-    
+    job_title = contacts_details["fields"][contact_id]["job_title"]
+    if job_title == "N/A":
+        contacts_details["fields"][contact_id]["job_title"] = None
+    else:
+        contacts_details["fields"][contact_id]["job_title"] = job_title.capitalize()
+
--- jhmanager/service/contacts_directory/cleanup_contact.py
+++ jhmanager/service/contacts_directory/cleanup_contact.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, session, request, redirect, flash
 
 
-def cleanup_contact_details(contact_details, contact_id):
-    update_contact = None
-    contact_details["fields"][contact_id]
-
-    for contact_id, field in contact_details.fields.items():
-        raise ValueError(contact_id, field)
-
-
-
-    return update_contact
-
-
 def cleanup_full_name(full_name):
     updated_full_name = (" ".join([x.capitalize() for x in full_name.split(' ')]))
     return updated_full_name
 
 
 def cleanup_company_name(company_name):
-    updated_company_name = (" ".join([x.capitalize() for x in company_name.split(' ')]))
-    return updated_company_name
+    if company_name:
+        company_name = (" ".join([x.capitalize() for x in company_name.split(' ')]))
+    return company_name
+
+
+def cleanup_contact_details(contact_details, contact_id):
+    for heading, value in contact_details["fields"].items():
+        if value == "N/A":
+            contact_details["fields"][heading] = None
+
+    contact_details["fields"]["company_name"] = cleanup_company_name(contact_details["fields"]["company_name"])
+    contact_details["fields"]["full_name"] = cleanup_full_name(contact_details["fields"]["full_name"])
+    contact_details["fields"]["job_title"] = contact_details["fields"]["job_title"].capitalize()
+
+
+
