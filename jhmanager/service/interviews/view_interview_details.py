311d7e122f8894fd8e6cae443fae88cb80003fb6
Andi Folgado <ac.folgado@gmail.com>
2021-04-07 17:07:00 +0100

    Structure for 'view interview details' has been updated, but not complete.

--- jhmanager/service/interviews/view_interview_details.py
+++ jhmanager/service/interviews/view_interview_details.py
@@ -1,63 +1,62 @@
 from flask import Flask, render_template, session, request, redirect
-from jhmanager.service.cleanup_files.cleanup_datetime_display import cleanup_date_format
-from jhmanager.service.cleanup_files.cleanup_datetime_display import cleanup_time_format
-from jhmanager.service.cleanup_files.cleanup_interview_fields import cleanup_interview_type
-from jhmanager.service.cleanup_files.cleanup_interview_fields import cleanup_medium
 from jhmanager.service.cleanup_files.cleanup_interview_fields import cleanup_fields_for_single_interview
+from jhmanager.service.cleanup_files.cleanup_general_fields import replace_na_value_with_none
+from jhmanager.service.cleanup_files.cleanup_app_fields import cleanup_interview_stage
 from datetime import datetime, time
 
 
-def add_company_details(details, company):
-    details["company_details"] = {
-        "company_name" : company.name, 
-        "view_company_url": '/company/{}/view_company'.format(company.company_id)
-    }
-
-
 def display_interview_details(session, user_id, interviewsRepo, application_id, interview_id, applicationsRepo, companyRepo):
-    application_details = applicationsRepo.grabApplicationDetailsByApplicationID(application_id)
+    application = applicationsRepo.grabApplicationByID(application_id)
+    interview = interviewsRepo.grabInterviewByID(interview_id)
+    company = companyRepo.getCompanyById(application.company_id)
 
-    for application in application_details:
-        company_id = application[2]
-
-    company = companyRepo.getCompanyById(company_id)
-    company_name = company.name
-
-    details = {
-        "app_id": application_id, 
-        "interview_id": interview_id, 
-        "company_name": company_name,
-        "update_url": "/applications/{}/interview/{}/update_interview".format(application_id, interview_id),
-        "delete_url": "/applications/{}/interview/{}/delete_interview".format(application_id, interview_id),
-        "view_interview_prep_url": '/applications/{}/interview/{}/interview_preparation'.format(application_id, interview_id), 
-        "return_to_application": '/applications/{}'.format(application_id), 
-        "view_notes_url": '/applications/{}/view_application_notes'.format(application_id)
+    general_details = {
+        "application_details": {},
+        "company_details": {}, 
+        "links": {}
     }
 
-    interview = interviewsRepo.grabInterviewByID(interview_id)
-    interview_id = interview.interview_id
-    application_id = interview.application_id
-    medium = interview.medium
-    other_medium = interview.other_medium
+    general_details["company_details"] = {
+        "name": company.name, 
+        "description": replace_na_value_with_none(company.description), 
+        "location": replace_na_value_with_none(company.location)
+    }
 
-    interview_details = {}
+    general_details["application_details"] = {
+        "job_role": application.job_role, 
+        "job_description": replace_na_value_with_none(application.job_description), 
+        "interview_stage": cleanup_interview_stage(application.interview_stage)
+    }
+
+    general_details["links"] = {
+        "company_website": company.url,
+        "company_profile": '/company/{}/view_company'.format(company.company_id), 
+        "update_interview": "/applications/{}/interview/{}/update_interview".format(application_id, interview_id),
+        "delete_interview": "/applications/{}/interview/{}/delete_interview".format(application_id, interview_id),
+        "view_interview_prep_url": '/applications/{}/interview/{}/interview_preparation'.format(application_id, interview_id), 
+        "view_application": '/applications/{}'.format(application_id), 
+        "view_notes_url": '/applications/{}/view_application_notes'.format(application_id), 
+    }
+
+    other_medium = interview.other_medium
+    interview_details = {
+        "fields": {}
+    }
     interview_details["fields"] = {
         "interview_id": interview_id,
-        "date": interview.interview_date,
-        "time": interview.interview_time,
-        "interview_type": interview.interview_type,
-        "status": interview.status.capitalize(),
-        "medium": medium, 
-        "location": interview.location,
+        "date": interview.interview_date, 
+        "time": interview.interview_time, 
+        "interview_type": interview.interview_type, 
+        "status":  interview.status,
+        "medium": interview.medium, 
+        "location": interview.location, 
         "interviewer_names": interview.interviewer_names, 
-        "video_link": interview.video_link, 
         "extra_notes": interview.extra_notes,
         "contact_number": interview.contact_number, 
-        "past_dated": False, 
-        "display_video_link": False,
+        "past_dated": False,
+        "video_link": interview.video_link,
+        "display_video_link": False 
     }
-
     cleanup_fields_for_single_interview(interview_details, other_medium)
-    add_company_details(details, company)
 
-    return render_template("view_interview_details.html", details=details, interview_details=interview_details)
+    return render_template("view_interview_details.html", general_details=general_details, interview_details=interview_details)
