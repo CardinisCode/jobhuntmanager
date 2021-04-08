0718ccf5cb604a92895340522c474d6581f563de
Andi Folgado <ac.folgado@gmail.com>
2021-04-08 16:06:54 +0100

    Updated the interviews presented on the dashboard -> they're now displayed in a 3-col grid.

--- jhmanager/service/display_dashboard_content.py
+++ jhmanager/service/display_dashboard_content.py
@@ -7,13 +7,18 @@ from jhmanager.service.cleanup_files.cleanup_job_offer_fields import cleanup_job
 from jhmanager.service.cleanup_files.cleanup_interview_fields import cleanup_interview_fields
 
 
-def extract_and_display_job_offers(job_offers, companyRepo):
-    job_offer_details = None
+def extract_and_display_job_offers(user_id, jobOffersRepo, companyRepo):
+    job_offers = jobOffersRepo.getJobOffersByUserId(user_id)
+
+    job_offer_details = {
+        "offer_count": 0, 
+        "details": None
+    }
     offer_count = 0
     count_list = []
 
     if not job_offers: 
-        return (job_offer_details, offer_count)
+        return job_offer_details
 
     job_offer_details = {"details": {}}
     for offer in job_offers: 
@@ -38,23 +43,28 @@ def extract_and_display_job_offers(job_offers, companyRepo):
         }
         cleanup_job_offer(job_offer_details, offer_count)
 
+    job_offer_details["offer_count"] = offer_count
 
-    return (job_offer_details, offer_count)
+    return job_offer_details
 
 
-def extract_and_display_interviews(all_interviews, applicationsRepo, companyRepo): 
-    if not all_interviews: 
-        interview_details = None
-    else: 
-        interview_details = {}
+def extract_and_display_interviews(user_id, interviewsRepo, applicationsRepo, companyRepo): 
+    all_interviews = interviewsRepo.grabUpcomingInterviewsByUserID(user_id)
+
+    interview_details = {
+        "interviews_count": 0, 
+        "fields": None
+    }
+
+    count = 0
+    if all_interviews:
         interview_details["fields"] = {}
         for interview in all_interviews:
+            count += 1
             interview_id = interview.interview_id
             application_id = interview.application_id
             company_id = applicationsRepo.grabApplicationByID(application_id).company_id
             company_name = companyRepo.getCompanyById(company_id).name
-            view_more_url = '/applications/{}/interview/{}'.format(application_id, interview_id)
-            update_url = '/applications/{}/interview/{}/update_interview'.format(application_id, interview_id)
             other_medium = interview.other_medium
             
             interview_details["fields"][interview_id] = {
@@ -62,7 +72,6 @@ def extract_and_display_interviews(all_interviews, applicationsRepo, companyRepo
                 "time": interview.interview_time,
                 "company_name": company_name,
                 "location": interview.location,
-                "View_More": view_more_url, 
                 "status": interview.status,
                 "interview_type": interview.interview_type,
                 "interview_medium": interview.medium, 
@@ -70,10 +79,13 @@ def extract_and_display_interviews(all_interviews, applicationsRepo, companyRepo
                 "contact_number": interview.contact_number,
                 "interviewers": interview.interviewer_names,
                 "past_dated": False, 
-                "update_url": update_url, 
+                "View_More": '/applications/{}/interview/{}'.format(application_id, interview_id), 
+                "update_url": '/applications/{}/interview/{}/update_interview'.format(application_id, interview_id), 
                 "prepare_url": '/applications/{}/interview/{}/interview_preparation'.format(application_id, interview_id)
             }
             cleanup_interview_fields(interview_details, interview_id)
+
+    interview_details["interviews_count"] = count
 
     return interview_details
 
@@ -84,16 +96,12 @@ def create_dashboard_content(user_id, applicationsRepo, interviewsRepo, userRepo
     date_format = "%Y-%m-%d"
     date_str = current_date.strftime(date_format)
 
-    # Now to grab the values for the Applications & Interviews added on the current date (from the perspective SQL queries):
-    # Firstly: applications
+    job_offer_details = extract_and_display_job_offers(user_id, jobOffersRepo, companyRepo)
+    interview_details = extract_and_display_interviews(user_id, interviewsRepo, applicationsRepo, companyRepo)
+
+    # Now to grab the figures/stats we'll be displaying at the top of the dashboard:
     applications_today = applicationsRepo.grabTodaysApplicationCount(date_str, user_id)
     interviews_today = interviewsRepo.grabTodaysInterviewCount(date_str, user_id)
-    all_interviews = interviewsRepo.grabUpcomingInterviewsByUserID(user_id)
-    interview_details = extract_and_display_interviews(all_interviews, applicationsRepo, companyRepo)
-
-    job_offers = jobOffersRepo.getJobOffersByUserId(user_id)
-    job_offer_details = extract_and_display_job_offers(job_offers, companyRepo)[0]
-    job_offer_count = extract_and_display_job_offers(job_offers, companyRepo)[1]
 
     # Sadly SQLite doesn't have the functionality to return COUNT(*) from SQLite to Python
     # So we'll have manually count the number of rows returned from the SQL query:
@@ -101,17 +109,13 @@ def create_dashboard_content(user_id, applicationsRepo, interviewsRepo, userRepo
     for app in applications_today:
         app_today_count += 1
 
-    interviews_today_count = 0
-    for interview in interviews_today:
-        interviews_today_count += 1
-
     message = "All good!"
 
     display = {
         'current_date': current_date,
         "applications_today": app_today_count,
-        "interviews_today": interviews_today_count,
-        "job_offer_count": job_offer_count,
+        "interviews_today": interview_details["interviews_count"],
+        "job_offer_count": job_offer_details["offer_count"],
         "message": message,
         "interview_details": interview_details, 
         "job_offer_details": job_offer_details,
