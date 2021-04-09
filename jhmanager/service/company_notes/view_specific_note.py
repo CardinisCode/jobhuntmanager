5b77435be5f05248381dbddb319b4f362953a08c
Andi Folgado <ac.folgado@gmail.com>
2021-04-09 13:09:29 +0100

    Minor styling updates to 'view app note details' and 'view specific company note' pages.

--- jhmanager/service/company_notes/view_specific_note.py
+++ jhmanager/service/company_notes/view_specific_note.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template, session, request, redirect, flash
+from jhmanager.service.cleanup_files.cleanup_datetime_display import cleanup_date_format
+from datetime import datetime, time
 
 
 def display_company_note_details(company_id, company_note_id, companyRepo, companyNotesRepo, user_id):
     company = companyRepo.getCompanyById(company_id)
     note_details = companyNotesRepo.getCompanyNoteByID(company_note_id)
-    details = {
-        "company_name": company.name,
-        "company_notes_url": '/company/{}/view_all_company_notes'.format(company_id),
-        "company_profile_url": '/company/{}/view_company'.format(company_id), 
-        "date": note_details.entry_date, 
-        "subject": note_details.subject, 
-        "note_text": note_details.note_text, 
-        "update_url": '/company/{}/company_note/{}/update_note'.format(company_id, company_note_id),
-        "delete_note_url": '/company/{}/company_note/{}/delete_note'.format(company_id, company_note_id)
+
+    general_details = {
+        "links": {}, 
+        "note_details": {}
     }
 
-    return render_template("view_specific_company_note.html", details=details)
+    general_details["links"] = {
+        "view_company_notes": '/company/{}/view_all_company_notes'.format(company_id),
+        "view_company_profile": '/company/{}/view_company'.format(company_id), 
+        "update_note": '/company/{}/company_note/{}/update_note'.format(company_id, company_note_id),
+        "delete_note": '/company/{}/company_note/{}/delete_note'.format(company_id, company_note_id),
+        "add_new_note": '/company/{}/add_company_note'.format(company_id)
+    }
+
+    note_date_obj = datetime.strptime(note_details.entry_date, "%Y-%m-%d")
+
+    general_details["note_details"] = {
+        "company_name": company.name,
+        "date": cleanup_date_format(note_date_obj), 
+        "subject": note_details.subject, 
+        "note_text": note_details.note_text, 
+    }
+
+    return render_template("view_specific_company_note.html", general_details=general_details)
