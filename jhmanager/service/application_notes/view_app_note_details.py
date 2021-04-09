10554faf0e5d10cd4d69f1af9d4d4623f665d468
Andi Folgado <ac.folgado@gmail.com>
2021-04-09 12:38:05 +0100

    Minor update to the 'view app note details' -> both on the HTML page & in the Python code.

--- jhmanager/service/application_notes/view_app_note_details.py
+++ jhmanager/service/application_notes/view_app_note_details.py
@@ -1,31 +1,34 @@
 from flask import Flask, render_template, session, request, redirect
+from jhmanager.service.cleanup_files.cleanup_datetime_display import cleanup_date_format
+from datetime import datetime, time
 
 
 def display_application_note_details(application_id, app_notes_id, appNotesRepo, companyRepo):
     app_notes = appNotesRepo.getNoteByAppNoteID(app_notes_id)
     company_id = app_notes.company_id
-    company_name = companyRepo.getCompanyById(company_id).name
-    update_url =  '/applications/{}/app_notes/{}/update_note'.format(application_id, app_notes_id)
-    delete_note_url = "/applications/{}/app_notes/{}/delete_note".format(application_id, app_notes_id)
-    company_profile = '/company/{}/view_company'.format(company_id)
-    all_app_notes = '/applications/{}/view_application_notes'.format(application_id)
-    view_application = '/applications/{}'.format(application_id)
+    company = companyRepo.getCompanyById(company_id)
 
     general_details = {
-        "application_id": application_id,
-        "company_profile": company_profile,
-        "all_app_notes": all_app_notes,
-        "view_application": view_application
+        "links": {}, 
+        "note_details": {}
     }
 
-    note_details = {
+    general_details["links"] = {
+        "company_profile": '/company/{}/view_company'.format(company_id),
+        "all_app_notes": '/applications/{}/view_application_notes'.format(application_id),
+        "view_application": '/applications/{}'.format(application_id),
+        "update_note": '/applications/{}/app_notes/{}/update_note'.format(application_id, app_notes_id), 
+        "delete_note": "/applications/{}/app_notes/{}/delete_note".format(application_id, app_notes_id), 
+    }
+
+    note_date_obj = datetime.strptime(app_notes.entry_date, "%Y-%m-%d")
+
+    general_details["note_details"] = {
         "note_id": app_notes.app_notes_id,
-        "Date": app_notes.entry_date,
-        "company_name": company_name,
-        "Subject": app_notes.description, 
-        "Note": app_notes.notes_text, 
-        "update_url": update_url,
-        "delete_note_url": delete_note_url
+        "date": cleanup_date_format(note_date_obj),
+        "company_name": company.name,
+        "subject": app_notes.description, 
+        "note": app_notes.notes_text, 
     }
 
-    return render_template("view_app_note_details.html", note_details=note_details, general_details=general_details)
+    return render_template("view_app_note_details.html", general_details=general_details)
