577bb6d0b4bbae81064d3f7deb888717f695e178
Andi Folgado <ac.folgado@gmail.com>
2021-04-26 12:36:11 +0100

    Updated the 'view address book' file to be consistent with other files. Added conditions for 'view address book' so it only displays icons when there's a value to display.

--- jhmanager/service/address_book/view_address_book.py
+++ jhmanager/service/address_book/view_address_book.py
@@ -3,51 +3,54 @@ from jhmanager.service.cleanup_files.cleanup_addressbook_values import cleanup_c
 
 
 def display_address_book(user_id, companyRepo, contactRepo):
-    display = {
-        "company_directory": '/address_book/company_directory', 
-        "add_company": '/address_book/add_company', 
-        "empty_list": True
+    company_list = companyRepo.getTop8CompaniesByUserID(user_id)
+    company_contacts = {
+        "empty_list": True,
+        "fields": None
     }
 
-    company_list = companyRepo.getTop8CompaniesByUserID(user_id)
-
-    company_details = {}
     if company_list:
-        display["empty_list"] = False
-        company_details["fields"] = {}
+        company_contacts["empty_list"] = False
+        company_contacts["fields"] = {}
         for company in company_list:
             company_id = company.company_id
             view_company = '/company/{}/view_company'.format(company_id)
-            company_details["fields"][company_id] = {
+            company_contacts["fields"][company_id] = {
                 "name": company.name,
                 "description": company.description,
                 "industry": company.industry, 
+                "location": company.location,
                 "view_company": view_company
             }
 
     contacts_list = contactRepo.getTop8ContactsByUserID(user_id)
-    contacts_details = {
+    indiv_contacts = {
         "empty_list": True, 
+        "fields": None,
         "message": "No contacts to display", 
-        "view_contacts": '/address_book/contact_list',
-        "add_new_contact": '/address_book/contact_list/add_contact'
     }
     if contacts_list:
-        contacts_details["fields"] = {}
-        contacts_details["empty_list"] = False
-        contacts_details["message"] = "There are contacts to display"
+        indiv_contacts["fields"] = {}
+        indiv_contacts["empty_list"] = False
 
         for contact in contacts_list:
             contact_id = contact.contact_id
-            view_contact = '/address_book/contact_list/{}/view_contact'.format(contact_id)
-            contacts_details["fields"][contact_id] = {
+            indiv_contacts["fields"][contact_id] = {
                 "full_name": contact.full_name, 
                 "job_title": contact.job_title,
                 "company_name": contact.company_name, 
-                "view_contact": view_contact
+                "view_contact": '/address_book/contact_list/{}/view_contact'.format(contact_id)
             }
-            cleanup_contact_fields(contacts_details, contact_id)
+            cleanup_contact_fields(indiv_contacts, contact_id)
 
-    return render_template("view_address_book.html", display=display, company_details=company_details, contacts_details=contacts_details)
+    general_details = {}
+    general_details["links"] = {
+        "company_directory": '/address_book/company_directory', 
+        "add_company": '/address_book/add_company', 
+        "view_contacts": '/address_book/contact_list',
+        "add_new_contact": '/address_book/contact_list/add_contact'
+    }
+
+    return render_template("view_address_book.html", general_details=general_details, company_contacts=company_contacts, indiv_contacts=indiv_contacts)
 
 
